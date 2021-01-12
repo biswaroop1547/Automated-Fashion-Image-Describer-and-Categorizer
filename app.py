bcefedbeec356138e7406d7c1c393d36b093bb93
biswaroop1547 <biswaroop08@gmail.com>
2021-01-12 15:51:28 +0530

    now accepts url of image

--- app.py
+++ app.py
@@ -45,6 +45,18 @@ if image_data is None and image_url:
     try:
         response = requests.get(image_url)
         image_data = BytesIO(response.content)
-        st.write(prediction(from_url = False, uploader_image_data = image_data))
+        pred_text = " ".join(prediction(from_url = False, uploader_image_data = image_data))
+        pred_text = pred_text.replace("<start> ", "").replace(" <end>", ".").capitalize()
+        st.markdown(f"### {pred_text}")
     except:
         st.write("Please enter a valid URL")
+
+elif image_data:
+    # try:
+    st.write("hey")
+    image_data = BytesIO(image_data.read())
+    pred_text = " ".join(prediction(from_url = False, uploader_image_data = image_data))
+    pred_text = pred_text.replace("<start> ", "").replace(" <end>", ".").capitalize()
+    st.markdown(f"### {pred_text}")
+    # except:
+    #     st.write("Some unexpected error occured..")
